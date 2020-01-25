15bb92c7e7b1183a31121dbac8825da0ea2cdbbc
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2020-01-24 16:44:45 -0800

    using sphinx spelling extension
    - modified conf.py Makefile
    - removed spellcheck script

--- conf.py
+++ conf.py
@@ -42,7 +42,15 @@ release = '0.13.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.spelling'
+]
+
+# sphinxcontrib.spelling settings
+spelling_word_list_filename = [
+    'spell.en.pws'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
